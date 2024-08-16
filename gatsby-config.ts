52accf8ad08a2b44a64465c9a5ce84d371548506
Dom Clemmer <thisisdom4@gmail.com>
2024-08-16 17:14:07 -0400

    update deps

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,28 +10,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    {
-      resolve: 'gatsby-plugin-prettier-eslint',
-      options: {
-        prettier: {
-          patterns: [
-            '**/*.{css,scss,less}',
-            '**/*.{json,json5}',
-            '**/*.{graphql}',
-            '**/*.{md,mdx}',
-            '**/*.{html}',
-            '**/*.{yaml,yml}',
-          ],
-        },
-        eslint: {
-          patterns: '**/*.{js,jsx,ts,tsx}',
-          customOptions: {
-            fix: true,
-            cache: true,
-          },
-        },
-      },
-    },
     `gatsby-plugin-sass`,
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
